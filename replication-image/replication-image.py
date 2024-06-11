82e3b15df7a3e057710b053a99e2b44ea717020c
Joseph Livesey <joseph@semiotic.ai>
2024-06-11 15:53:19 -0400

    feat: configure replication container port via env

--- replication-image/replication-image.py
+++ replication-image/replication-image.py
@@ -4,6 +4,7 @@ import logging
 import image_pb2
 import image_pb2_grpc
 import json
+import os
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
@@ -29,10 +30,13 @@ class ImageServiceServicer(image_pb2_grpc.ImageServiceServicer):
 
 
 def serve():
-    logging.info("Starting 'replication-image' server. Listening on port 50051.")
+    port = os.getenv('PORT')
+    if not port:
+        raise ValueError("PORT environment variable is not set.")
+    logging.info(f"Starting 'replication-image' server. Listening on port {port}.")
     server = grpc.server(ThreadPoolExecutor(max_workers=10))
     image_pb2_grpc.add_ImageServiceServicer_to_server(ImageServiceServicer(), server)
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port(f"[::]:{port}")
     server.start()
     server.wait_for_termination()
 

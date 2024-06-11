59ac47619ce60f3f9b01e218a7219c5e20070d88
Joseph Livesey <joseph@semiotic.ai>
2024-06-11 17:27:26 -0400

    feat: move toward supporting image and text output types

--- replication-ctl/src/main.rs
+++ replication-ctl/src/main.rs
@@ -10,21 +10,6 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tonic::transport::Endpoint;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct IntentConfig {
-    model: String,
-    input: Value,
-}
-
-impl From<IntentConfig> for ReplicateRequest {
-    fn from(IntentConfig { model, input }: IntentConfig) -> Self {
-        ReplicateRequest {
-            model,
-            input: input.to_string(),
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let matches = cli().get_matches();
@@ -42,21 +27,67 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let response = client.process_intent(request).await?.into_inner();
 
-    let payload = response.payload;
+    match response.output_type.as_str().into() {
+        ResponseType::Image => {
+            let payload = response.payload;
 
-    if let Some(url) = payload.get(0) {
-        // Download the image data from the first URL
-        let image_data = reqwest::get(url).await?.bytes().await?;
+            if let Some(string) = payload.into_iter().next() {
+                let image_data = reqwest::get(&string).await?.bytes().await?;
 
-        // Save the image to a file
-        std::fs::write("received_image.png", &image_data)?;
+                std::fs::write("received_image.png", &image_data)?;
 
-        println!("Image saved as received_image.png");
-    } else {
-        println!("No URLs received in the response.");
+                println!("Image saved as received_image.png");
+            } else {
+                eprintln!("Nothing received in the response.");
+            }
+
+            println!("\nReceived response from replication service:\n");
+        }
+        ResponseType::Text => unimplemented!(),
+        ResponseType::Unrecognized => eprintln!("Error! Unrecognized response type!"),
     }
 
-    println!("\nReceived response from 'replication' via Replicate:\n");
-
     Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct IntentConfig {
+    model: String,
+    input: Value,
+    input_type: String,
+    output_type: String,
+}
+
+impl From<IntentConfig> for ReplicateRequest {
+    fn from(
+        IntentConfig {
+            model,
+            input,
+            input_type,
+            output_type,
+        }: IntentConfig,
+    ) -> Self {
+        ReplicateRequest {
+            model,
+            input: input.to_string(),
+            input_type,
+            output_type,
+        }
+    }
+}
+
+enum ResponseType {
+    Image,
+    Text,
+    Unrecognized,
+}
+
+impl From<&str> for ResponseType {
+    fn from(s: &str) -> Self {
+        match s {
+            "image" => Self::Image,
+            "text" => Self::Text,
+            _ => Self::Unrecognized,
+        }
+    }
 }
